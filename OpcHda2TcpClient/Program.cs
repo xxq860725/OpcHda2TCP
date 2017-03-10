87441e556a10610bb5bae57add42301fe7206611
zh <zh@TOMMEYVERCETTI>
2017-03-10 11:59:22 +0800

    修改解决方案名称

--- OpcHda2TcpClient/Program.cs
+++ OpcHda2TcpClient/Program.cs
@@ -11,7 +11,7 @@ namespace OpcHda2TcpClient
 {
 	class Program
 	{
-		static int ll = 0;
+		//static int ll = 0;
 		static List<byte> reciveData = new List<byte> { };
 		static void Main(string[] args)
 		{
@@ -19,55 +19,15 @@ namespace OpcHda2TcpClient
 			bool isConnected=myClient.Connect();
 			if (isConnected)
 			{
+				//发送命令到服务器，命令长度较短，暂时没有考虑传输错误
 				myClient.SyncSend("111222333444555666777888999000");
+				//读取服务器返回的包头
 				myClient.ReadHeader();
+				//
 				myClient.AsyncRead();
 			}
 			Console.ReadLine();
 			myClient.Close();
-			//TcpClient myClient = new TcpClient();
-			//myClient.ReceiveBufferSize = 16 * 1024;
-			//myClient.Connect(IPAddress.Parse("127.0.0.1"), 3000);
-			////发送数据
-			//NetworkStream ns = myClient.GetStream();
-			//string message = "12345678901234567890" + "\r\n";
-			//List<byte> data = new List<byte> { };
-			//data.AddRange(Encoding.UTF8.GetBytes(message));
-			//ns.Write(data.ToArray(), 0, data.Count);
-			////接收数据
-			//byte[] buffer = new byte[myClient.ReceiveBufferSize];
-			//ns.BeginRead(buffer, 0, buffer.Length, asyncread, myClient);
-			//Console.WriteLine("按回车退出.....");
-			//Console.ReadLine();		
-			//ns.Close();
-			//myClient.Close();
-
-		}
-		private static void asyncread(IAsyncResult ar)
-		{
-			TcpClient client = (TcpClient)ar.AsyncState;
-			if (!client.Connected) return;
-			NetworkStream stream = client.GetStream();
-			int recv = 0;
-			try
-			{
-				recv = stream.EndRead(ar);
-			}
-			catch
-			{
-				recv = 0;
-			}
-			ll += recv;
-			Console.WriteLine("ll={0}", ll);
-			byte[] buff = new byte[recv];
-			stream.Read(buff, 0, recv);
-			reciveData.AddRange(buff);
-			//Buffer.BlockCopy(state.Buffer, 0, buff, 0, recv);
-			//触发数据收到事件
-			//RaiseDataReceived(state);
-			// continue listening for tcp datagram packets
-			buff = new byte[client.ReceiveBufferSize];
-			stream.BeginRead(buff, 0, buff.Length, asyncread, client);
 		}
 	}
 }
