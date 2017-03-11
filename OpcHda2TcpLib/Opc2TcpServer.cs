b3d00b86eccbebfd47a87c77867e6c82f8154ae9
zh <zh@TOMMEYVERCETTI>
2017-03-11 21:21:10 +0800

    更新类，暂时不用，觉得没有必要，先保留

--- OpcHda2TcpLib/Opc2TcpServer.cs
+++ OpcHda2TcpLib/Opc2TcpServer.cs
@@ -1,46 +1,97 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using OpcHda2Tcp;
-using System.Net;
-using System.IO;
 using System.Data;
-using System.Web.Script.Serialization;
-using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace OpcHda2TcpServer
+namespace OpcHda2Tcp
 {
-	class Program
+	/// <summary>
+	/// 暂时先不用这个类，觉得多余。
+	/// </summary>
+	public class Opc2TcpServer
 	{
-		private static AsyncTCPServer myTcpServer = new AsyncTCPServer(IPAddress.Parse("127.0.0.1"), 3000);//监听本机3000端口
-		private static OPCHDAClient myOpcClient = new OPCHDAClient("", "OPCServerHDA.WinCC.1");
-		static void Main(string[] args)
-		{			
-			#region 事件注册
+		#region 私有静态字段
+		/// <summary>
+		/// tcp server
+		/// </summary>
+		private static AsyncTCPServer myTcpServer ;//监听本机3000端口
+		/// <summary>
+		/// opc client
+		/// </summary>
+		private static OPCHDAClient myOpcClient;
+		#endregion
 
+		/// <summary>
+		/// opc server 主机名
+		/// </summary>
+		private string _OpcServerHostName = "";
+
+		/// <summary>
+		/// opc server 名
+		/// </summary>
+		private string _OpcServerName = "";
+
+		/// <summary>
+		/// tcp server 监听的ip
+		/// </summary>
+		private string _LocalIP = "";
+
+		/// <summary>
+		/// tcp server 监听的端口
+		/// </summary>
+		private int _ListenPort = 0;
+
+		/// <summary>
+		/// 构造函数
+		/// </summary>
+		/// <param name="OpcServerHostName"></param>
+		/// <param name="OpcServerName"></param>
+		/// <param name="LocalIP"></param>
+		/// <param name="ListenPort"></param>
+		public Opc2TcpServer(string OpcServerHostName="" ,string OpcServerName= "OPCServerHDA.WinCC.1",string LocalIP="127.0.0.1",int ListenPort=3000)
+		{
+			_OpcServerHostName = OpcServerHostName;
+			_OpcServerName = OpcServerName;
+			if (_OpcServerHostName == "") _OpcServerHostName = Dns.GetHostName();
+			if (_OpcServerName == "") _OpcServerName = "OPCServerHDA.WinCC.1";
+			myOpcClient = new OPCHDAClient(_OpcServerHostName, _OpcServerName);
+			_LocalIP = LocalIP;
+			_ListenPort = ListenPort;
+			if (ListenPort <= 0) _ListenPort = 3000;
+			IPAddress address;		
+			try
+			{
+				address = IPAddress.Parse(_LocalIP);
+			}
+			catch
+			{
+				address = IPAddress.Parse("127.0.0.1");
+			}
+			myTcpServer = new AsyncTCPServer(address, _ListenPort);
+			//注册事件
 			myTcpServer.ClientConnected += MyTcpServer_ClientConnected;
-
 			myTcpServer.DataReceived += MyTcpServer_DataReceived;
-
 			myTcpServer.CompletedSend += MyTcpServer_CompletedSend;
-
 			myTcpServer.ClientDisconnected += MyTcpServer_ClientDisconnected;
-			
-			#endregion
+		}
 
+		public void Start()
+		{
 			myTcpServer.Start();
 			Console.WriteLine("服务器已经启动，正在监听中.....");
-			while (true)//程序停在这里
-			{
-				string arg = Console.ReadLine();
-				if (arg == "exit") break;
-			}
-
 		}
+		
+		/// <summary>
+		/// 客户端断开连接事件
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		private static void MyTcpServer_ClientDisconnected(object sender, AsyncEventArgs e)
 		{
-			Console.WriteLine(  "客户端断开连接，当前连接数："+myTcpServer.ClientCount);
+			Console.WriteLine("客户端断开连接，当前连接数：" + myTcpServer.ClientCount);
 		}
 
 		/// <summary>
@@ -65,7 +116,7 @@ namespace OpcHda2TcpServer
 			byte[] length = System.BitConverter.GetBytes(data.Length);
 			byte[] header = new byte[48];
 			System.Security.Cryptography.MD5 md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
-			byte[] hash= md5.ComputeHash(data);
+			byte[] hash = md5.ComputeHash(data);
 			System.Buffer.BlockCopy(length, 0, header, 0, 4);
 			System.Buffer.BlockCopy(hash, 0, header, 4, 16);
 			//发送头包
@@ -103,7 +154,7 @@ namespace OpcHda2TcpServer
 		/// </summary>
 		/// <param name="state"></param>
 		/// <param name="data"></param>
-		private static void sendDatatoClient(TCPClientState state,byte[] data )
+		private static void sendDatatoClient(TCPClientState state, byte[] data)
 		{
 			myTcpServer.Send(state, data);
 		}
@@ -116,9 +167,9 @@ namespace OpcHda2TcpServer
 		/// <param name="endtime"></param>
 		/// <param name="step"></param>
 		/// <param name="tags"></param>
-		private static void analisisCmd(string cmd,out string starttime, out string endtime, out string step, out List<string> tags)
+		private static void analisisCmd(string cmd, out string starttime, out string endtime, out string step, out List<string> tags)
 		{
-			starttime = endtime=step=string.Empty;
+			starttime = endtime = step = string.Empty;
 			tags = new List<string> { };
 			if (cmd.Split('%').Length != 4)
 			{
@@ -126,8 +177,8 @@ namespace OpcHda2TcpServer
 				return;
 			}
 			starttime = cmd.Split('%')[0];
-			endtime= cmd.Split('%')[1];
-			step= cmd.Split('%')[2];
+			endtime = cmd.Split('%')[1];
+			step = cmd.Split('%')[2];
 			tags.AddRange(cmd.Split('%')[3].Split(','));
 		}
 
@@ -139,14 +190,14 @@ namespace OpcHda2TcpServer
 		/// <param name="step"></param>
 		/// <param name="tags"></param>
 		/// <returns></returns>
-		private static byte[] readDataFromOpc(string starttime,string endtime,string step,List<string>tags)
+		private static byte[] readDataFromOpc(string starttime, string endtime, string step, List<string> tags)
 		{
 			string serverName = "OPCServerHDA.WinCC.1";
 			//HDA主机名
 			string hostName = ""; //"WIN-NVSKV8UAC6U";
 			hostName = Dns.GetHostName();
 			//创建一个客户端
-			var hdac = new OPCHDAClient(hostName,serverName);//OPCHDA.Client();
+			var hdac = new OPCHDAClient(hostName, serverName);//OPCHDA.Client();
 			try
 			{
 				bool _connected = hdac.Connect();
@@ -160,14 +211,14 @@ namespace OpcHda2TcpServer
 				//读取数据到datatable    
 				DataTable dt = hdac.ReadByInterval(start, end, int.Parse(step), true);
 				//序列化为json
-				
+
 				string strJson = Util.DataTableToJson(dt);
 				hdac.Disconnect();
-				strJson =Util. GZipCompressString(strJson);
+				strJson = Util.GZipCompressString(strJson);
 				return Encoding.UTF8.GetBytes(strJson);
 			}
 			catch (Exception ex)
-			{				
+			{
 				Console.WriteLine(ex.Message);
 				return new byte[1];
 			}
